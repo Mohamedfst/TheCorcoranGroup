a3c855cd8e9eca231f5154333ca4e300a1418305
Mohamed <keita.momo1@gmail.com>
2018-10-31 15:12:55 -0700

    h1

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ app.use(bodyParser.json());
 
 app.use('/api/presidents', presidents);
 
-// Port 8080 for Google App Engine
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app;
