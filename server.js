a8fee459dd502088422871d382385858fefed0be
Mohamed <keita.momo1@gmail.com>
2018-11-01 16:31:24 -0700

    n

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/presidents', presidents);
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname,'corcoran' ,'client', 'build', 'index.html'));
     });
 
 const port = process.env.PORT;
