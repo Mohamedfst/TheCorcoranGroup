db261c8c7c22dc02225ceb53917d8698907c5328
Mohamed <keita.momo1@gmail.com>
2018-11-01 16:22:02 -0700

    s

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(bodyParser.json());
 app.use('/api/presidents', presidents);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+
     // Set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
